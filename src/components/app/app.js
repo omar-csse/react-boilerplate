d0c2e16a703b568e268b9b4de189d09af9ca7a08
omar <omar.csse@gmail.com>
2019-09-25 23:58:18 +1000

    add template files

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.css'
+
+const App = () => {
+    return(
+        <div className="app">Omar</div>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/main.html',
+            favicon: './public/favicon.ico'
+        })
+    ]
+}
